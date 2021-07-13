42c2c3c1a435b75e6de6c857cdb832b89b8af3d5
DingZeAn <andy856996@gamil.com>
2021-07-13 21:45:13 +0800

    chenge the color of the logo and the nav

--- assets/js/main_mod.js
+++ assets/js/main_mod.js
@@ -2,8 +2,20 @@ $(function(){
         $(window).scroll(function(){
          if($(this).scrollTop() > 50) {
           $('#header').addClass('active');
+
+          $('.navbar a').addClass('navbar_white');
+          $('.navbar a').removeClass('navbar_other');
+
+          $('#header .logo h1 a').removeClass('logo_color');
+          $('#header .logo h1 a').addClass('logo_color_mod');
          } else {
           $('#header').removeClass('active');
+
+          $('.navbar a').removeClass('navbar_white');
+          $('.navbar a').addClass('navbar_other');
+
+          $('#header .logo h1 a').removeClass('logo_color_mod');
+          $('#header .logo h1 a').addClass('logo_color');
          }
       });
 
