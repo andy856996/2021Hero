679a493467ac1e7584bbddfbd00025a3d7277f4e
DingZeAn <andy856996@gmail.com>
2021-07-28 19:08:42 +0800

    commit a Activity_process part

--- script_shapes_circle.js
+++ script_shapes_circle.js
@@ -1,9 +1,30 @@
+  mo =1;
 document.body.addEventListener("mousemove", evt => {
   const mouseX = evt.clientX;
   const mouseY = evt.clientY;
 
-  gsap.to(".shape_circle", {
-    x: mouseX,
-    y: mouseY,
-    stagger: -0.1 });
+  if($(window).width() < 800)
+  {
+      // JavaScript here 
+      // 當視窗寬度小於800px時執行
+    
+    setTimeout(function(){
+        if(mo==3){
+          mo=1;
+        }
+        gsap.to(".shape_circle", {
+        x: $(window).width()/mo,
+        y: $(window).height()/mo,
+        });
+        mo=mo+1;
+    },3000);
+  } else {
+      gsap.to(".shape_circle", {
+        x: mouseX,
+        y: mouseY,
+      stagger: -0.1 });
+      // JavaScript here
+      // 當視窗寬度不小於800px時執行
+  }
+
 });
